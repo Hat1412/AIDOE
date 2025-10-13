1d512c933009f495e7b4997482bb08e3d91b6c2c
Hat1412 <hari8haran8@gmail.com>
2025-10-13 13:09:48 +0530

    MAde website

---
+++ register.js
@@ -0,0 +1,38 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDTKxBcIcBktiGz6PCKxc06u27FTKVRq2g",
+    authDomain: "aidoe-fa943.firebaseapp.com",
+    projectId: "aidoe-fa943",
+    storageBucket: "aidoe-fa943.firebasestorage.app",
+    messagingSenderId: "16541357001",
+    appId: "1:16541357001:web:e315c71dc3a4b65ab48e18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const submit = document.getElementById('Reg_Button')
+
+submit.addEventListener('click', function (e) {
+    e.preventDefault()
+    const email = document.getElementById('Reg_Email').value;
+    const password = document.getElementById('Reg_Password').value;
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+
+});
